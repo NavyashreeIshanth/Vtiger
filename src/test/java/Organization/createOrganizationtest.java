c063a53c9b020d7b1b7501d98b62ac1139ffbf7d
Navya <Admin@m>
2022-12-24 11:57:06 +0530

    TC_215-->committed

---
+++ src/test/java/Organization/createOrganizationtest.java
@@ -0,0 +1,15 @@
+
+ 
+package Organization;
+
+import org.testng.annotations.Test;
+
+public class createOrganizationtest {
+
+	@Test
+	public void tc_215_CreateOrganizationwithMandatoryField()
+	{
+		System.out.println("TC_215");
+	}
+	
+}
